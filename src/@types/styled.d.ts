bb5c05e44e5e1556fc06808f35a351389b35cda1
Thainá Pires <thainaspiress@gmail.com>
2023-02-27 23:03:44 -0300

    add type definition for themes

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,9 @@
+//Arquivo de definição de tipos
+import 'styled-components';
+import { defaultTheme } from '../styles/themes/default';
+
+type ThemeType = typeof defaultTheme;
+
+declare module 'styled-components' {
+    export interface DefaultTheme extends ThemeType {}
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Button } from "./components/Button";
-
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from "./styles/themes/default";
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary"/>
       <Button variant="secondary"/>
       <Button variant="success"/>
       <Button variant="danger"/>
       <Button />
-    </>
+    </ThemeProvider>
   )
 }
