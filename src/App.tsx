8156c7d86eda5f5548391837a97930a3418429f9
Thainá Pires <thainaspiress@gmail.com>
2023-02-27 23:06:20 -0300

    add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Button } from "./components/Button";
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
@@ -9,6 +11,8 @@ export function App() {
       <Button variant="success"/>
       <Button variant="danger"/>
       <Button />
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
