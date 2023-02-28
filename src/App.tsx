99ad6cb13ae084c63f86d80bba1017afd322e138
Thainá Pires <thainaspiress@gmail.com>
2023-02-27 22:47:28 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/Button";
+
+export function App() {
+  return (
+    <>
+      <Button variant="primary"/>
+      <Button variant="secondary"/>
+      <Button variant="success"/>
+      <Button variant="danger"/>
+      <Button />
+    </>
+  )
+}
