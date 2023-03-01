7d3aa840e7f4ee57b93b8fc48ff13d549f4b3054
Thainá Pires <thainaspiress@gmail.com>
2023-02-28 22:20:27 -0300

    add code to customize it more

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,9 +1,9 @@
-//Arquivo de definição de tipos
-import 'styled-components';
-import { defaultTheme } from '../styles/themes/default';
+// Arquivo de definição de tipos
+import 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
 
-type ThemeType = typeof defaultTheme;
+type ThemeType = typeof defaultTheme
 
 declare module 'styled-components' {
-    export interface DefaultTheme extends ThemeType {}
+  export interface DefaultTheme extends ThemeType {}
 }
